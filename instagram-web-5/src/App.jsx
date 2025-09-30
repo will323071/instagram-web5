4eaa3a28fefa5f1295003a4a0e86a5d479e2b98c
cylee323071@gmail.com <cylee323071@gmail.com>
2025-09-30 16:26:41 +0900

    B

--- instagram-web-5/src/App.jsx
+++ instagram-web-5/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <hi>instagram</hi>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
